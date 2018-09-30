2bd3301125a5fdbde6c80eb32b13caac983ce9e5
gjcarrow <gjcarrow@gmail.com>
2018-09-30 02:09:41 -0400

    edit service worker

--- sw.js
+++ sw.js
@@ -19,13 +19,11 @@ self.addEventListener('activate', event=>{
 })
 
 self.addEventListener('fetch', async event=>{
-	console.log('sw fetch')
 	const request = event.request
 	const url = new URL(request.url)
 	if(url.origin === location.origin) {
 		event.respondWith(cacheFirst(request))
 	} else {
-		console.log(request)
 		event.respondWith(networkFirst(request))
 	}
 })
@@ -43,31 +41,6 @@ async function networkFirst (request) {
 		return networkResponse
 	} catch(e) {
 		const cachedResponse = await dynamicCache.match(request)
-		console.log('This is the catch block of the networkFirst method: ', e);
 		return cachedResponse || await caches.match('./fallback.json')
 	}
 }
-
-
-
-
-
-
-
-
-
-
-/*this.addEventListener('fetch', function(event) {
-  var response;
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
-  }).then(function(r) {
-    response = r;
-    caches.open('v1').then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }).catch(function() {
-    return caches.match('/sw-test/gallery/myLittleVader.jpg');
-  }));
-});*/
